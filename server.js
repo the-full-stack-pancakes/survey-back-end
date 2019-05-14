b6c48897c2911781d425bd3aac86e661b10e0013
JeanAugustin <jre.augustin@gmail.com>
2019-05-14 12:23:55 -0400

    Resolved conflict on server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+
 const questionRoutes = require('./app/routes/question_routes')
 
 // require error handling middleware
@@ -61,6 +62,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+
 app.use(questionRoutes)
 
 // register error handling middleware
